9ab6352b18976bb74b943c3d813c7e3c8f4581f2
TheCountryGamer <the.country.gamer@gmail.com>
2014-08-04 13:53:05 -0400

    Syntax

--- src/main/java/com/countrygamer/racesforminecraft/api/package-info.java
+++ src/main/java/com/countrygamer/racesforminecraft/api/package-info.java
@@ -1,3 +1,4 @@
-@API(owner = "racesforminecraft", apiVersion = "0.1", provides = "RacesForMinecraft|API") package com.countrygamer.racesforminecraft.api;
+@API(owner = "racesforminecraft", apiVersion = "0.1",
+		provides = "RacesForMinecraft|API") package com.countrygamer.racesforminecraft.api;
 
 import cpw.mods.fml.common.API;

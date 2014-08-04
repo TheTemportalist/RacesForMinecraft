93bd8f055a31ef6a65d18286b7d4cd0679b79773
TheCountryGamer <the.country.gamer@gmail.com>
2014-08-04 10:40:28 -0400

    Setting up for reflection

--- src/main/java/com/countrygamer/racesforminecraft/RfMAPI.java
+++ src/main/java/com/countrygamer/racesforminecraft/RfMAPI.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
  *
  * @author CountryGamer
  */
+@Deprecated
 public class RfMAPI {
 
 	public static Race getRace(EntityPlayer player) {
---
+++ src/main/java/com/countrygamer/racesforminecraft/common/APIHelper.java
@@ -0,0 +1,44 @@
+package com.countrygamer.racesforminecraft.common;
+
+import com.countrygamer.racesforminecraft.common.talent.AbstractTalent;
+import com.countrygamer.racesforminecraft.common.talent.Caste;
+import com.countrygamer.racesforminecraft.common.talent.Race;
+import com.countrygamer.racesforminecraft.common.talent.Skill;
+import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.HashSet;
+
+/**
+ * @author CountryGamer
+ */
+public class APIHelper {
+
+	public static Race getRace(EntityPlayer player) {
+		return null;
+	}
+
+	public static HashSet<Skill> getSkills(EntityPlayer player) {
+		return null;
+	}
+
+	public static HashSet<Caste> getCastes(EntityPlayer player) {
+		return null;
+	}
+
+	public static void applyTalent(EntityPlayer player, AbstractTalent talent) {
+
+	}
+
+	public static void applyTalent(EntityPlayer player, String talentName) {
+
+	}
+
+	public static void revokeTalent(EntityPlayer player, AbstractTalent talent) {
+
+	}
+
+	public static void revokeTalent(EntityPlayer player, String talentName) {
+
+	}
+
+}
